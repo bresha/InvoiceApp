0bc92e45c5e3aaeeb0659883985df536f35ed638
Hrvoje Brešić <hrvoje.bresic@gmail.com>
2020-12-19 11:09:40 +0100

    Seed Admin Role to database

---
+++ InvoiceApp/Constants/Roles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvoiceApp.Constants
+{
+    public static class Roles
+    {
+        public const string Admin = "Admin";
+    }
+}
